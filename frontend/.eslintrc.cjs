af5c810867764c2e4780dd3b12bc80ce5638ac16
bella77-69 <mrspasceri@hotmail.com>
2025-09-06 18:39:33 -0700

    add eslint, prettier, folders

---
+++ frontend/.eslintrc.cjs
@@ -0,0 +1,12 @@
+module.exports = {
+  parser: '@typescript-eslint/parser',
+  extends: [
+    'react-app',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:react-hooks/recommended',
+    'prettier'
+  ],
+  plugins: ['@typescript-eslint'],
+  rules: {
+  },
+};
